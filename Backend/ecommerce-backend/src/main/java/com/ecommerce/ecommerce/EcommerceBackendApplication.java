a4512cc4cf77bbcd5a51ecfc4f581b53f062507f
Daniel Anaya <68717105+Daniel123-dot-pixel@users.noreply.github.com>
2024-11-10 22:56:09 -0500

    Add files via upload

---
+++ Backend/ecommerce-backend/src/main/java/com/ecommerce/ecommerce/EcommerceBackendApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/ecommerce-backend/src/main/java/com/ecommerce/ecommerce/entity/Product.java
@@ -0,0 +1,73 @@
+package com.ecommerce.ecommerce.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "precio")
+    private BigDecimal precio;
+
+    @Column(name = "stock")
+    private int stock;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+}
